08d0fcbcdca7b89451c80d644825978214893e33
Jordan Larios <jolarios96@gmail.com>
2018-11-07 18:15:07 -0800

    game.js recoded using objects, methods, ...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,65 @@
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var guessesMade = [];
-
 var letters = "abcdefghijklmnopqrstuvwxyz";
-
-// alternatively:
-    // var solution = generateSolution(),
-    // but first solution will be undefined.
 var solution = letters.charAt(Math.floor(Math.random() * 26));
-
 console.log("Game Solution: " + solution);
+
+
+var player = {
+    wins: 0,
+    losses: 0,
+    guessesLeft: 9,
+    guessesMade: [],
+
+    reset: function () {
+        this.guessesLeft = 9;
+        this.guessesMade = [];
+        solution = letters.charAt(Math.floor(Math.random() * 26));
+    },
+
+    updateScore: function () {
+        document.getElementById("scoreboard").innerHTML =
+            "<p>Wins: " + this.wins + "</p>" +
+            "<p>Losses: " + this.losses + "</p>" +
+            "<p>Guesses Left: " + this.guessesLeft + "</p>" +
+            "<p>Your Guesses so far: " + this.guessesMade.join(", ") + "</p>";
+    },
+};
 
 document.onkeyup = function (event) {
     var userInput = event.key;
 
-    // Game Loop
+    // Game Logic
     if (letters.indexOf(userInput) > -1) {
         // Restricts pressable keys to lowercase letters only.
-            // When userInput is not found in the string of letters, or index of letters, final value = -1
+        // When userInput is not found in the string of letters, or index of letters, final value = -1
 
         // Data Debug Log
-        console.log("Loop: " + (10 - guessesLeft) + " | Guess #: " + (guessesLeft) + " | Wins: " + wins + " | Losses: " + losses);
+        console.log("Loop: " + (10 - player.guessesLeft) + " | Guess #: " + (player.guessesLeft) + " | Wins: " + player.wins + " | Losses: " + player.losses);
 
         // Reset on Win
         if (userInput === solution) {
-            wins++;
-            guessesLeft = 9;
-            guessesMade = [];
-            generateSolution();
+            player.wins++;
+            player.reset();
             console.log("-- Game Won --");
             console.log("-- Resetting --");
             console.log("Game Solution: " + solution);
         }
         // Retry on Miss
         else {
-            guessesLeft--;
-            guessesMade.push(userInput);
+            player.guessesLeft--;
+            player.guessesMade.push(userInput);
             console.log("-- Try Again --");
         }
 
         // Reset on Loss
-        if (guessesLeft === 0) {
-            guessesLeft = 9;
-            losses++;
-            guessesMade = [];
-            generateSolution();
+        if (player.guessesLeft === 0) {
+            player.losses++;
+            player.reset();
             console.log("-- Game Lost --");
             console.log("-- Resetting --");
             console.log("Game Solution: " + solution);
         }
 
-        // Update Score after any Step
-        document.getElementById("scoreboard").innerHTML =
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + losses + "</p>" +
-            "<p>Guesses Left: " + guessesLeft + "</p>" +
-            "<p>Your Guesses so far: " + guessesMade.join(", ") + "</p>";
+        // Update Score after any one Step
+        player.updateScore();
     }
-}
-
-function generateSolution() {
-    solution = letters.charAt(Math.floor(Math.random() * 26));
-}
+};
